8c19ea540f4ffe99c14574d4941ee041a18dff7a
shivam3596 <shivamchauhan3596@gmail.com>
2020-09-08 14:14:59 +0530

    indentation and other fixes

--- 15-infix-to-postfix-using-stack.c
+++ 15-infix-to-postfix-using-stack.c
@@ -36,61 +36,57 @@ void Pop(){
 
 bool IsOperand(char C)
 {
-	if(C >= '0' && C <= '9') return true;
-	if(C >= 'a' && C <= 'z') return true;
-	if(C >= 'A' && C <= 'Z') return true;
-	return false;
+    if(C >= '0' && C <= '9') return true;
+    if(C >= 'a' && C <= 'z') return true;
+    if(C >= 'A' && C <= 'Z') return true;
+    return false;
 }
 
 bool IsOperator(char C)
 {
-	if(C == '+' || C == '-' || C == '*' || C == '/' || C== '$')
-		return true;
+    if(C == '+' || C == '-' || C == '*' || C == '/' || C== '$')
+        return true;
 
-	return false;
+    return false;
 }
 
 int IsRightAssociative(char op)
 {
-	if(op == '$') return true;
-	return false;
+    if(op == '^') return true;
+    return false;
 }
 
 // Function to get weight of an operator. An operator with higher weight will have higher precedence.
 int GetOperatorWeight(char op)
 {
-	int weight = -1;
-	switch (op)
-    {
-    case '+':
-    case '-':
-        return 1;
-
-    case '*':
-    case '/':
-        return 2;
-
-    case '^':
-        return 3;
+    int weight = -1;
+    switch (op){
+        case '+':
+        case '-':
+            return 1;
+        case '*':
+        case '/':
+            return 2;
+        case '^':
+            return 3;
     }
-	return weight;
+    return weight;
 }
 
 // Function to perform an operation and return output.
 int HasHigherPrecedence(char op1, char op2)
 {
-	int op1Weight = GetOperatorWeight(op1);
-	int op2Weight = GetOperatorWeight(op2);
+    int op1Weight = GetOperatorWeight(op1);
+    int op2Weight = GetOperatorWeight(op2);
 
-	// If operators have equal precedence, return true if they are left associative.
-	// return false, if right associative.
-	// if operator is left-associative, left one should be given priority.
-	if(op1Weight == op2Weight)
-	{
-		if(IsRightAssociative(op1)) return false;
-		else return true;
-	}
-	return op1Weight > op2Weight ?  true: false;
+    // If operators have equal precedence, return true if they are left associative.
+    // return false, if right associative.
+    // if operator is left-associative, left one should be given priority.
+    if(op1Weight == op2Weight){
+        if(IsRightAssociative(op1)) return false;
+        else return true;
+    }
+    return op1Weight > op2Weight ?  true: false;
 }
 
 void InfixToPostFix(char* C, int length){
