321308adebb8a096810d389dbc90922ef6fc7509
shivam3596 <shivamchauhan3596@gmail.com>
2020-09-23 22:54:20 +0530

    first and last occurance of element in array

---
+++ Algorithms/04-find-first-and-last-occurance-of-element.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int FirstOccurance(int* array, int total, int find){
+    int start = 0;
+    int end = total - 1;
+    int result = -1;
+
+    while(start <= end){
+        int mid = start+(end-start)/2;
+
+        if(find == array[mid]){
+            result = mid;
+            end = mid - 1;
+        }else if(find < array[mid]){
+            end = mid - 1;
+        }else{
+            start = mid + 1;
+        }
+    }
+    return result;
+}
+
+int LastOccurance(int* array, int total, int find){
+    int start = 0;
+    int end = total - 1;
+    int result = -1;
+
+    while(start <= end){
+        int mid = start+(end-start)/2;
+
+        if(find == array[mid]){
+            result = mid;
+            start = mid + 1;
+        }else if(find < array[mid]){
+            end = mid - 1;
+        }else{
+            start = mid + 1;
+        }
+    }
+    return result;
+}
+
+void main(){
+    int i,total,number,find;
+
+    printf("enter total: ");
+    scanf("%d",&total);
+
+    int array[total];
+
+    for(i=0; i<total; i++){
+        printf("enter the number: ");
+        scanf("%d",&number);
+        array[i] = number;
+    }
+
+    printf("enter the number you want to search: ");
+    scanf("%d",&find);
+
+    printf("first occurance is at: %d\n",FirstOccurance(array,total,find));
+    printf("last occurance is at: %d\n",LastOccurance(array,total,find));
+}
